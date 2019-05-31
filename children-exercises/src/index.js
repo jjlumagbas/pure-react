2b42deb020d3603bd368723a38bc0183eb3fa61e
JJ Lumagbas <jjlumagbas@gmail.com>
2019-05-31 15:26:56 -0500

    Chidren exercises 2

--- children-exercises/src/index.js
+++ children-exercises/src/index.js
@@ -1,12 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function ErrorBox({ children }) {
+  return (
+    <div className="error">
+      <i className="fas fa-exclamation-triangle"></i>
+      <span className="error-message">{children}</span>
+    </div>
+  );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function FirstChildOnly({ children }) {
+  return (
+    React.Children.toArray(children)[0]
+  );
+}
+
+function LastChildOnly({ children }) {
+  const childrenArr = React.Children.toArray(children);
+  return (
+    childrenArr[childrenArr.length - 1]
+  );
+}
+
+function Head({ number, children }) {
+  return (
+    React.Children.toArray(children).slice(0, number)
+  );
+}
+
+function Tail({ number, children }) {
+  const length = React.Children.count(children);
+  return (
+    React.Children.toArray(children).slice(length - number, length)
+  );
+}
+
+ReactDOM.render(
+  <>
+    <ErrorBox>Something has gone wrong</ErrorBox>
+    <FirstChildOnly><p>first</p><p>second</p></FirstChildOnly>
+    <LastChildOnly><p>first</p><p>second</p></LastChildOnly>
+    <Head number={3}><p>first</p><p>second</p><p>third</p></Head>
+    <Tail number={2}><p>first</p><p>second</p><p>third</p></Tail>
+  </>,
+  document.getElementById('root')
+);
+
