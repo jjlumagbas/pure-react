c031d10c9856b4b116c0ab73244accede3b645bb
JJ Lumagbas <jjlumagbas@gmail.com>
2019-06-01 10:29:03 -0500

    Filelist markup and styling

--- github-filelist/src/index.js
+++ github-filelist/src/index.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function FileName() {
+  return (
+    <h2 className='filename'>
+      <a href='/#'>
+        <i class="fileicon fas fa-folder"></i>
+        build
+      </a>
+    </h2>
+  );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function FileListItem() {
+  return (
+    <article className='filelist-item'>
+      <FileName />
+      <p>Close #13784</p>
+      <footer class='time'>2 days ago</footer>
+    </article>
+  );
+}
+
+function FileList() {
+  return (
+    <div className='filelist'>
+      <FileListItem />
+      <FileListItem />
+    </div>
+  )
+}
+
+ReactDOM.render(<FileList />, document.getElementById('root'));
+
