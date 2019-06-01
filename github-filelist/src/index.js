1a9ffd08081f7d3eb7b9763e17b89ad7c37e8a94
JJ Lumagbas <jjlumagbas@gmail.com>
2019-06-01 10:48:20 -0500

    Filelist icon and filename props and proptypes

--- github-filelist/src/index.js
+++ github-filelist/src/index.js
@@ -1,35 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 import './index.css';
 
-function FileName() {
+function FileIcon({ file }) {
+  const icon = file.type === 'folder' ? 'fa-folder' : 'fa-file';
+  return (
+    <i className={`fileicon fas ${icon}`}></i>
+  );
+}
+
+function FileName({ file }) {
   return (
     <h2 className='filename'>
       <a href='/#'>
-        <i class="fileicon fas fa-folder"></i>
-        build
+        <FileIcon file={file} />
+        {file.name}
       </a>
     </h2>
   );
 }
 
-function FileListItem() {
+
+function FileListItem({ file }) {
   return (
     <article className='filelist-item'>
-      <FileName />
+      <FileName file={file} />
       <p>Close #13784</p>
-      <footer class='time'>2 days ago</footer>
+      <footer className='time'>2 days ago</footer>
     </article>
   );
 }
 
+FileListItem.propTypes = {
+  file: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    type: PropTypes.oneOf(['file', 'folder']).isRequired
+  })
+};
+
 function FileList() {
   return (
-    <div className='filelist'>
-      <FileListItem />
-      <FileListItem />
-    </div>
+    <section className='filelist'>
+      <FileListItem file={file1} />
+      <FileListItem file={file2} />
+    </section>
   )
+}
+
+const file1 = {
+  name: 'source',
+  type: 'folder'
+}
+
+const file2 = {
+  name: 'index.html',
+  type: 'file'
 }
 
 ReactDOM.render(<FileList />, document.getElementById('root'));
