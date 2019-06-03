ab9f1686969b919c52a13f00279c3351896fab61
JJ Lumagbas <jjlumagbas@gmail.com>
2019-06-03 11:28:43 -0500

    Parent passes function to child

--- state/src/index.js
+++ state/src/index.js
@@ -2,4 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function handleAction(event) {
+  console.log('child did:', event);
+}
+
+function Parent() {
+  return (
+    <Child onAction={handleAction} />
+  );
+}
+
+function Child({ onAction }) {
+  return (
+    <button onClick={onAction}>
+      Click Me!
+    </button>
+  );
+}
+
+ReactDOM.render(<Parent />, document.getElementById('root'));
