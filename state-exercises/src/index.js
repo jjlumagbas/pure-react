7adc7fa35ec9938a2a92df0fbe748dcc6bf9920f
JJ Lumagbas <jjlumagbas@gmail.com>
2019-11-13 14:40:22 -0600

    State exercises init

---
+++ state-exercises/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Page = () => (
+  <div>
+    Hello
+  </div>
+);
+
+ReactDOM.render(<Page />, document.getElementById('root'));
